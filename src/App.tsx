37ea91a70612fe0f547e5ac104d9ff66d724f340
Isaías ramos <isaiasramosdeoliverira2003@gmail.com>
2023-01-14 01:13:19 -0300

    Ajustes nos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import { useEffect, useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import Assignment from './components/Assignment/Assignment';
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import styles from './style/scss/App.module.scss';
 import Home from './pages/Home';
 
-
+let state = false
+export const Context = React.createContext(state);
 function App() {
-  const [value, setValue] = useState(false);
-  useEffect((): void =>{
-    const handleLoandingPage = (): void =>{
-      setValue(value)
-    }
-    handleLoandingPage()
-  }, [value])
-  
+  const [value, setValue] = useState<Boolean>(state);
   return (
     <main>
-      {value? <Menu/> : <Home/>}
+      <Context.Provider value={{value, setValue} as any}>
+        {value? <Menu/> : <Home value={{value, setValue}} />}
+      </Context.Provider>
     </main>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState , useContext, useEffect} from 'react'
 import styles from "./styles/Home/Home.module.scss";
-
 import logo from './../../src/assets/img/logo.png';
+import { Context } from '../App';
 
-const Home = ({handleLoandingPage, value}: any) => {
-  const [state, setState] = useState(value)
+const Home = ({handleLoandingPage}: any) => {
+  const {value, setValue} = useContext(Context) as any
+  const handleActive = (): void =>{
+    setValue(true)
+  } 
   return (
     <section className={styles.container}>
       <div className={styles.menu}>
-        <a href=""><i  onClick={() => setState(true)} className="fa-solid fa-xmark"></i></a>
+        <a href=""><i onClick={handleActive} className="fa-solid fa-xmark"></i></a>
       </div>
       <div className={styles.apresentation}>
         <picture>
@@ -16,7 +19,7 @@ const Home = ({handleLoandingPage, value}: any) => {
         </picture>
         <h1>Open Space</h1>
         <h5>O lugar certo para você organizar sua vida.</h5>
-        <button onClick={() =>setState(true)}>Começar</button>
+        <button onClick={handleActive}>Começar</button>
       </div>
     </section>
   )
